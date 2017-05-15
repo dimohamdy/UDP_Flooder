8126c37d50ed43e167f43aaaccc85ef7a5c3574b
dimohamdy <dimo.hamdy@gmail.com>
2017-05-15 19:20:31 +0200

    Initial Commit

---
+++ UDP_Flooder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UDP_Flooder
+//
+//  Created by BinaryBoy on 5/15/17.
+//  Copyright © 2017 BinaryBoy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
